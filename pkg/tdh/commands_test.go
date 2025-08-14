d141657500a562b9e1b8f4613c1a831d69549123
adebert <debert@gmail.com>
2025-08-13 23:06:06 -0300

    test: remove empty commands_test.go file
    All tests have been moved to their respective command packages,
    so this file is no longer needed.

--- pkg/tdh/commands_test.go
+++
@@ -1,6 +0,0 @@
-package tdh_test
-
-// This file is now empty as all command tests have been moved to their respective packages:
-// - TestAddCommand -> pkg/tdh/commands/add/add_test.go
-// - TestToggleCommand -> pkg/tdh/commands/toggle/toggle_test.go
-// - TestCleanCommand -> pkg/tdh/commands/clean/clean_test.go
