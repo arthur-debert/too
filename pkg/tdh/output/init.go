c0e047bc6ee03fc52d070ba5632efb5d016bc6ae
adebert <debert@gmail.com>
2025-08-16 19:33:31 -0300

    feat: Add Markdown output formatter

--- pkg/tdh/output/init.go
+++ pkg/tdh/output/init.go
@@ -3,5 +3,6 @@ package output
 // Import all built-in formatters to ensure they register themselves
 import (
 	_ "github.com/arthur-debert/tdh/pkg/tdh/output/formatters/json"
+	_ "github.com/arthur-debert/tdh/pkg/tdh/output/formatters/markdown"
 	_ "github.com/arthur-debert/tdh/pkg/tdh/output/formatters/term"
 )
